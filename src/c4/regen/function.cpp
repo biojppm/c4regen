650430faf2fd46bfdcc01ef286d0bae239f9fa6b
Joao Paulo Magalhaes <dev@jpmag.me>
2018-06-11 14:55:27 +0100

    function: move function defs to source file

--- src/c4/regen/function.cpp
+++ src/c4/regen/function.cpp
@@ -1,9 +1,32 @@
 #include "c4/regen/function.hpp"
 
+#include <c4/c4_push.hpp>
+
 namespace c4 {
 namespace regen {
 
+void FunctionParameter::init_param(astEntityRef e, Function c$ f)
+{
+    this->Entity::init(e);
+}
+
+void Function::init(astEntityRef e)
+{
+    m_entity_type = ENT_FUNCTION;
+    this->TaggedEntity::init(e);
+    int num_args = clang_Cursor_getNumArguments(m_cursor);
+    m_parameters.resize(num_args);
+    unsigned i = 0;
+    for(auto &a : m_parameters)
+    {
+        ast::Cursor pc = clang_Cursor_getArgument(m_cursor, i++);
+        ast::Entity pe{pc, m_cursor, m_tu, m_index};
+        a.init_param(pe, this);
+    }
+}
 
 
 } // namespace regen
 } // namespace c4
+
+#include <c4/c4_pop.hpp>
--- src/c4/regen/function.hpp
+++ src/c4/regen/function.hpp
@@ -12,14 +12,9 @@ namespace regen {
 struct Function;
 struct FunctionParameter : public Entity
 {
-    Function c$ m_function{nullptr};
-    DataType    m_data_type;
+    DataType m_data_type;
 
-    void init_param(astEntityRef e, Function c$ f)
-    {
-        m_function = f;
-        this->Entity::init(e);
-    }
+    void init_param(astEntityRef e, Function c$ f);
 };
 
 
@@ -31,20 +26,7 @@ struct Function : public TaggedEntity
     DataType m_return_type;
     std::vector<FunctionParameter> m_parameters;
 
-    virtual void init(astEntityRef e) override
-    {
-        m_entity_type = ENT_FUNCTION;
-        this->TaggedEntity::init(e);
-        int num_args = clang_Cursor_getNumArguments(m_cursor);
-        m_parameters.resize(num_args);
-        unsigned i = 0;
-        for(auto &a : m_parameters)
-        {
-            ast::Cursor pc = clang_Cursor_getArgument(m_cursor, i++);
-            ast::Entity pe{pc, m_cursor, m_tu, m_index};
-            a.init_param(pe, this);
-        }
-    }
+    virtual void init(astEntityRef e) override;
 };
 
 
@@ -54,10 +36,6 @@ struct Function : public TaggedEntity
 struct FunctionGenerator : public Generator
 {
     FunctionGenerator() : Generator() { m_entity_type = ENT_FUNCTION; }
-    void create_prop_tree(Entity const& o, c4::yml::NodeRef root) const override
-    {
-        C4_NOT_IMPLEMENTED();
-    }
 };
 
 
