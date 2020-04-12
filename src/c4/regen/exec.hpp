bf5ee25a78450c3e699236f026a499fd1240b34f
Joao Paulo Magalhaes <dev@jpmag.me>
2020-04-13 01:54:02 +0200

    exec: avoid copying the Regen object

--- src/c4/regen/exec.hpp
+++ src/c4/regen/exec.hpp
@@ -18,7 +18,7 @@ const option::Descriptor usage[] =
     {CMD    , 0, "x", "cmd"  , c4::opt::required, "  -x <cmd>, --cmd=<cmd>  \t(required) The command to execute. Must be one of [generate,outfiles]." },
     {CFG    , 0, "c", "cfg"  , c4::opt::required, "  -c <cfg-yml>, --cfg=<cfg-yml>  \t(required) The full path to the regen config YAML file." },
     {DIR    , 0, "d", "dir"  , c4::opt::nonempty, "  -d <build-dir>, --dir=<build-dir>  \tThe full path to the directory containing the compile_commands.json file." },
-    {FLAGS  , 0, "f", "flag" , c4::opt::nonempty, "  -f <compiler-flag>, --flag=<compiler-flag>  \tThe full path to the directory containing the compile_commands.json file." },
+    {FLAGS  , 0, "f", "flag" , c4::opt::nonempty, "  -f <compiler-flag>, --flag=<compiler-flag>  \tAdd a flag to pass to the compiler, generally --flag '-x' --flag 'c++' should be used." },
     {0,0,0,0,0,0}
 };
 
@@ -30,7 +30,7 @@ inline bool valid_cmd(csubstr cmd)
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
-inline int exec(int argc, const char *argv[], bool skip_exe_name=false, regen::Regen *results=nullptr)
+inline int exec(regen::Regen *rg, int argc, const char *argv[], bool skip_exe_name=false)
 {
     using namespace c4;
 
@@ -41,12 +41,10 @@ inline int exec(int argc, const char *argv[], bool skip_exe_name=false, regen::R
     }
 
     auto opts = opt::make_parser(usage, argc, argv, HELP, {CFG});
-    regen::Regen ws;
-    regen::Regen &rg = (results != nullptr) ? *results : ws;
 
-    rg.load_config(opts(CFG));
+    rg->load_config(opts(CFG));
 
-    if(rg.empty()) return 0;
+    if(rg->empty()) return 0;
 
     csubstr cmd = to_csubstr(opts(CMD));
     C4_CHECK(valid_cmd(cmd));
@@ -55,7 +53,7 @@ inline int exec(int argc, const char *argv[], bool skip_exe_name=false, regen::R
     {
         if(opts[DIR])
         {
-            rg.gencode(opts.posn_args(), opts[DIR].arg);
+            rg->gencode(opts.posn_args(), opts[DIR].arg);
         }
         else
         {
@@ -64,30 +62,51 @@ inline int exec(int argc, const char *argv[], bool skip_exe_name=false, regen::R
             {
                 flags.push_back(f.arg);
             }
-            rg.gencode(opts.posn_args(), nullptr, flags.data(), flags.size());
+            rg->gencode(opts.posn_args(), nullptr, flags.data(), flags.size());
         }
     }
     else if(cmd == "outfiles")
     {
-        rg.print_output_filenames(opts.posn_args());
+        rg->print_output_filenames(opts.posn_args());
+    }
+    else
+    {
+        C4_ERROR("unknown command: .*%s", (int)cmd.len, cmd.str);
     }
 
     return 0;
 }
 
-template <size_t N>
-inline int exec(const char *(&argv)[N], bool skip_exe_name=false, regen::Regen *results=nullptr)
+inline int exec(int argc, const char *argv[], bool skip_exe_name=false)
 {
-    return exec(N, argv, skip_exe_name, results);
+    regen::Regen results;
+    return exec(&results, argc, argv, skip_exe_name);
 }
 
-inline int exec(std::initializer_list<const char*> il, bool skip_exe_name=false, regen::Regen *results=nullptr)
+template <size_t N>
+inline int exec(regen::Regen *results, const char *(&argv)[N], bool skip_exe_name=false)
 {
+    return exec(results, N, argv, skip_exe_name);
+}
+template <size_t N>
+inline int exec(const char *(&argv)[N], bool skip_exe_name=false)
+{
+    regen::Regen results;
+    return exec(&results, N, argv, skip_exe_name);
+}
+
+inline int exec(regen::Regen *results, std::initializer_list<const char*> il, bool skip_exe_name=false)
+{
+    int  argc = static_cast<int>(il.size());
     // [support.initlist] 18.9/1 specifies that for std::initializer_list<T>
     // iterator must be T*
     auto argv = const_cast<const char**>(il.begin());
-    int  argc = static_cast<int>(il.size());
-    return exec(argc, argv, skip_exe_name, results);
+    return exec(results, argc, argv, skip_exe_name);
+}
+inline int exec(std::initializer_list<const char*> il, bool skip_exe_name=false)
+{
+    regen::Regen results;
+    return exec(&results, il, skip_exe_name);
 }
 
 } // namespace regen
